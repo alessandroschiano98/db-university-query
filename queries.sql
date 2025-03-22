0a398f10d3d5e6325a7541100a27dc77e707ffee
alessandroschiano98 <schianoski98@gmail.com>
2025-03-22 12:49:55 +0100

    ESERCIZI DA FARE (5-6)

--- queries.sql
+++ queries.sql
@@ -12,7 +12,9 @@ SELECT *
 FROM `students`
 WHERE `date_of_birth` LIKE "1995%"; 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
-
+SELECT *
+FROM `courses`
+WHERE `period` = "I semestre" AND `year` = 1;
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20-06-2020 (21)
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
@@ -35,3 +37,5 @@ UPDATE teachers
 SET office_number = 126
 WHERE id = 58;
 -- 11. Eliminare dalla tabella studenti il record creato precedentemente al punto 9
+DELETE FROM `students` 
+WHERE `degree_id` = 5 AND email = 'schianoski98@gmail.com';
